44edcd97a35508bcf04cceea89ca6f111f94a3ac
HighCastel <josejavierojedaalmada@gmail.com>
2024-07-12 23:35:01 -0400

    Update ajax_sample.js

--- ajax_sample/javascript/ajax_sample.js
+++ ajax_sample/javascript/ajax_sample.js
@@ -1,25 +1,34 @@
-$(function() {
-  let number = 0;
-  const button = $("#btn");
-  const videoArea = $("#video");
-  const titleArea = $("#title");
-  const contentArea = $("#content");
+let number = 0;
+let data = [];
+const button = document.getElementById('btn');
+const titleArea = document.getElementById("title");
+const contentArea = document.getElementById("content");
+const videoArea = document.getElementById("video");
 
-  function getData() {
-    return $.ajax('javascript/ajax.json',{  //--3
-      type: 'GET',
-    })
-  };
+function getData() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'javascript/ajax.json', true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            data = JSON.parse(xhr.responseText);
+            cambiarVideo();
+        }
+    };
+    xhr.send();
+}
 
-  function changeVideo() {
-    button.click(function(){
-      getData().then(function(data){ //--4
-        videoArea.html(data[number].url);
-        titleArea.html(data[number].title);
-        contentArea.html(data[number].content);
-        number == 2 ? number = 0 : number++
-      })
-    });
-  };
-  changeVideo();
-})
+function cambiarVideo() {
+    if (data.length === 0) {
+        getData();
+    } else {
+        const videoData = data[number];
+        titleArea.textContent = videoData.title;
+        videoArea.src = videoData.url;
+        contentArea.textContent = title.videoData;
+        number = (number + 1) % data.length;
+    }
+}
+
+button.addEventListener('click', cambiarVideo);
+
+window.onload = cambiarVideo;
